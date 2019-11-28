b0f17b3874ffdc63e7ba222282e420b8e451862b
Jeanne GOT <jeanne.got@irisa.fr>
2019-11-28 12:19:44 +0100

    Add bk_to_faa

--- aureme/utils.py
+++ aureme/utils.py
@@ -3,6 +3,7 @@
 """
 Usage:
     aureme utils --run=ID --compounds_to_sbml --cpd=/path/to/root/txt_file [-v]
+    aureme utils --run=ID --gbk_to_faa --gbk=/path/to/root/txt_file [-v]
 
 options:
     --run=ID    Pathname to the aureme directory.
@@ -20,6 +21,8 @@ import re
 import subprocess
 import time
 
+from padmet.utils.connection import sbmlGenerator, gbk_to_faa
+
 from aureme.parse import parse_config_file
 
 
@@ -34,26 +37,43 @@ def utils_parse_args(command_args):
 
     if args['--compounds_to_sbml'] and args['--cpd']:
             txt_file = args['--cpd']
-            compounds_to_sbml(run_id, txt_file, verbose)
+            compounds_to_sbml_utils(run_id, txt_file, verbose)
+            return
+    if args['--gbk_to_faa'] and args['--gbk']:
+        gbk_file = args['--gbk']
+        gbk_to_faa_utils(run_id, gbk_file, verbose)
 
-    
-def compounds_to_sbml(species, txt_file, verbose):
+        
+def compounds_to_sbml_utils(species, input_file, verbose):
     """
-    It read the txt_file and it creates a sbml file.
+    It reads the input_file at TXT format and it creates a SBML file.
     """
     run_id = "/shared/" + species
-    config_data = parse_config_file(run_id)
-    padmet_u = config_data['padmet_u']
-    root_file = run_id + "/" + txt_file
-
-    if os.path.exists(root_file + ".txt"):
-        print('"------>RUNNING STEP : creating file %s.sbml' %txt_file)
-        cmds = ["python3.7", padmet_u + "/connection/sbmlGenerator.py",
-                "--compound=" + root_file + ".txt",
-                "--output=" + root_file + ".sbml"]
+    root_file = run_id + "/" + input_file
+    txt_file = root_file + ".txt"
+    sbml_file = root_file + ".sbml"
+    if os.path.exists(txt_file):
         if verbose:
-            cmds.append('-v')
-            subprocess.call(cmds)
+            print("------>RUNNING STEP : creating file %s.sbml" %input_file)
+        sbmlGenerator.compound_to_sbml(txt_file, sbml_file, verbose)
     else:
-        print(root_file + ".txt is missing.")
+        print(txt_file + " is missing.")
+        exit()
+
+        
+def gbk_to_faa_utils(species, input_file, verbose):
+    """
+    It reads the input_file at GBK format and it creates a FASTA file.
+    """
+    run_id = "/shared/" + species
+    root_file = run_id + "/" + input_file
+    gbk_file = root_file + ".gbk"
+    faa_file = root_file + ".faa"
+    if os.path.exists(gbk_file):
+        if verbose:
+            print("------>RUNNING STEP : gbk to faa")
+        gbk_to_faa.gbk_to_faa(gbk_file=gbk_file, output=faa_file,
+                              verbose=verbose)
+    else:
+        print(faa_file + " is missing.")
         exit()
